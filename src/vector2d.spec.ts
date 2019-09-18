bf3436ccf5d8df042e0848eeeec6e3b27998b141
Tom A. Vibeto <tom@onlyhuman.dk>
2019-09-18 23:42:55 +0200

    add some tests.

--- src/vector2d.spec.ts
+++ src/vector2d.spec.ts
@@ -1,5 +1,15 @@
 import Vector2d from './vector2d';
 
-test('is the tester running', () => {
+test('tester should run', () => {
 	expect(true).toEqual(true);
+});
+
+test('a Vector2d object should be created', () => {
+	expect(new Vector2d(0,0)).toBeDefined();
+	expect(new Vector2d(0,0)).toBeTruthy();
+});
+
+test('should create a vector2d object with default values', () => {
+	expect(new Vector2d()).toBeDefined();
+	expect(new Vector2d()).toBeTruthy();
 });
