7e4590f8c03e339e71eb7bb285cf62d8f01760c0
Tom A. Vibeto <tom@onlyhuman.dk>
2019-09-23 23:28:58 +0200

    Add a few tests.

--- src/vector2d.test.ts
+++ src/vector2d.test.ts
@@ -1,4 +1,7 @@
-import { assert } from 'chai';
+import {
+	assert,
+	expect
+} from 'chai';
 import Vector2d from './vector2d';
 
 describe('Typescript mocha test runner', () => {
@@ -6,7 +9,13 @@ describe('Typescript mocha test runner', () => {
 		assert.equal(true, true);
 	});
 	it('should create a vector2d object', () => {
-		assert.equal(!!new Vector2d(), true);
-		assert.typeOf(new Vector2d(), 'object');
+		expect(new Vector2d()).to.be.an('object');
+		expect(new Vector2d()).to.have.property('x').equal(0);
+		expect(new Vector2d()).to.have.property('y').equal(0);
+	});
+
+	it('should serialize to string', () => {
+		expect(new Vector2d().toString()).to.be.a('string');
+		expect(new Vector2d().toString()).to.equal('(0, 0)');
 	});
 });
